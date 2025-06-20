1f7e8401c828f3542dcef0ed0130b4e092b081ca
Davidkuguk <46162029+Davidkuguk@users.noreply.github.com>
2025-06-20 12:25:04 +0200

    añadimos apariencias y props

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import MenuItem from "./components/menuItem";
+import { menuItems } from "./data/db"
+
 function App() {
 
 
@@ -7,6 +10,28 @@ function App() {
       <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumos</h1>
     </header>
       
+    <main className="max-w-7xl mx-auto py-20 px-20 grid md:grid-cols-2">
+      <div>
+        <h2 className="text-4xl font-black">Menu</h2>
+        <div className="space-y-3 mt-10">
+          {menuItems.map(item => (
+
+          <MenuItem
+           key={item.id}    
+           item={item}
+          
+          />
+        
+        ))}
+        </div>
+        
+      </div>
+      
+      <div className="text-4xl font-black">
+        <h2>Consumo</h2>
+      </div>
+      
+    </main>
     </>
   )
 }
